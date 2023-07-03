37070047305a785c073a642e1ac2ca786d96f723
Mayank Tripathi <84989130+MAYANK-TRIPATH@users.noreply.github.com>
2023-07-03 10:58:13 +0530

    Context API

---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { ConterContext } from "../context/conter";
+
+const Counter = () => {
+    const counterContext = useContext(ConterContext);
+  return (
+    <div>
+        <button onClick={() => counterContext.setCount(counterContext.count + 1)}>
+            Increment</button>
+        <button onClick={() => counterContext.setCount(counterContext.count - 1)}>Decrement  </button>
+    </div>
+  )
+}
+
+export default Counter
